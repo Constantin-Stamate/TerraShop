a7eea4eefe084914abb44898ef34e427d31a0501
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-03-20 19:56:03 +0200

    configure error page layout and implement return view

--- eUseControl.Web/Controllers/MainController.cs
+++ eUseControl.Web/Controllers/MainController.cs
@@ -13,5 +13,11 @@ namespace eUseControl.Web.Controllers
         {
             return View();
         }
+
+        // GET: Error
+        public ActionResult Error404()
+        {
+            return View();
+        }
     }
 }
