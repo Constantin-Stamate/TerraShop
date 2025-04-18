6550193f6a6f67c07861f4455b4686d944a4ad74
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-04-18 20:36:13 +0300

    implement functionality for user registration, session management and cookie generation

---
+++ eUseControl.BusinessLogic/SessionBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using eUseControl.BusinessLogic.Core;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities;
+
+namespace eUseControl.BusinessLogic
+{
+    public class SessionBL : UserApi, ISession
+    {
+        public URegisterResp UserRegister(URegisterData data)
+        {
+            return UserRegisterAction(data);
+        }
+
+        public HttpCookie GenCookie(string loginCredential)
+        {
+            return Cookie(loginCredential);
+        }
+
+        public UserMinimal GetUserByCookie(string apiCookieValue)
+        {
+            return UserCookie(apiCookieValue);
+        }
+    }
+}
