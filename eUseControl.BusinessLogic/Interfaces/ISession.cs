07331994ed9b9501bc102c6650eaa8aec5751a12
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-04-18 20:19:05 +0300

    define ISession interface for user registration and cookie handling

---
+++ eUseControl.BusinessLogic/Interfaces/ISession.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eUseControl.Domain.Entities;
+using System.Web;
+
+namespace eUseControl.BusinessLogic.Interfaces
+{
+    public interface ISession
+    {
+        URegisterResp UserRegister(URegisterData data);
+
+        HttpCookie GenCookie(string loginCredential);
+
+        UserMinimal GetUserByCookie(string apiCookieValue);
+    }
+}
