0829860b2c06dfa88b35810aab50c262f343fb67
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-04-19 16:48:26 +0300

    update and add entities for authentication process

--- eUseControl.Domain/Entities/User/ULoginData.cs
+++ eUseControl.Domain/Entities/User/ULoginData.cs
@@ -5,20 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 using eUseControl.Domain.Enums;
 
-namespace eUseControl.Domain.Entities
+namespace eUseControl.Domain.Entities.User
 {
-    public class UserMinimal
+    public class ULoginData
     {
-        public int Id { get; set; }
-
         public string Username { get; set; }
 
-        public string Email { get; set; }
-
-        public DateTime LastLogin { get; set; }
+        public string Password { get; set; }
 
         public string LastIp { get; set; }
 
         public URole Level { get; set; }
+
+        public DateTime LastLogin { get; set; }
     }
 }
