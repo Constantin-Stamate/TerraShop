40d2da2e9c253c50f5a4be863fddd3743f0e2d91
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-04-25 22:14:47 +0300

    add migrations and seed categories for product and category tables

---
+++ eUseControl.BusinessLogic/Migrations/Configuration.cs
@@ -0,0 +1,40 @@
+﻿namespace eUseControl.BusinessLogic.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using eUseControl.Domain.Entities.Product;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<eUseControl.BusinessLogic.DBModel.CategoryContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(eUseControl.BusinessLogic.DBModel.CategoryContext context)
+        {
+            context.Categories.AddOrUpdate(
+                c => c.CategoryName,
+                new Category { CategoryName = "Personal Care" },
+                new Category { CategoryName = "Household" },
+                new Category { CategoryName = "Kitchen" },
+                new Category { CategoryName = "Eco Fashion" },
+                new Category { CategoryName = "Organic Cosmetics" },
+                new Category { CategoryName = "Health Care" },
+                new Category { CategoryName = "Eco Chemicals" },
+                new Category { CategoryName = "Renewable Energy" },
+                new Category { CategoryName = "Biodegradable Tableware" },
+                new Category { CategoryName = "Eco Transport" },
+                new Category { CategoryName = "Eco Car Cleaning" },
+                new Category { CategoryName = "Eco Construction" },
+                new Category { CategoryName = "Pet Care" },
+                new Category { CategoryName = "Eco Gardening" },
+                new Category { CategoryName = "Eco Stationery" }
+            );
+
+            context.SaveChanges();
+        }
+    }
+}
