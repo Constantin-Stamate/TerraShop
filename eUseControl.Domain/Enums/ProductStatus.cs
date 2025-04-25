e35bfff15b8463891ad6b8f8cd8364bea1888f19
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-04-25 21:58:10 +0300

    add new enum for product status representation

---
+++ eUseControl.Domain/Enums/ProductStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eUseControl.Domain.Enums
+{
+    public enum ProductStatus
+    {
+        Available = 10,
+        Unavailable = 20
+    }
+}
