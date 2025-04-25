59954c37e69fbbebc8221a8ff70355341c491112
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-04-25 22:10:22 +0300

    create DbContexts for product and category management

---
+++ eUseControl.BusinessLogic/DBModel/CategoryContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eUseControl.Domain.Entities;
+using eUseControl.Domain.Entities.Product;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    public class CategoryContext : DbContext
+    {
+        public CategoryContext() : base("name=eUseControl")
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; }
+    }
+}
