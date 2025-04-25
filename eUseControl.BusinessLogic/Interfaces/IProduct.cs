26cf6a1fbdbd70dbd1f823b91a00ff85d9a42480
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-04-25 22:05:23 +0300

    declare and implement methods for product management

---
+++ eUseControl.BusinessLogic/Interfaces/IProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eUseControl.Domain.Entities.Product;
+
+namespace eUseControl.BusinessLogic.Interfaces
+{
+    public interface IProduct
+    {
+        ProductResp CreateProduct(ProductData productData, string cookie);
+
+        List<ProductMinimal> GetProductsByUser(string cookie);
+
+        ProductResp UpdateProduct(ProductData productData, string cookie, int productId);
+
+        ProductData GetProductById(int productId, string cookie);
+    }
+}
