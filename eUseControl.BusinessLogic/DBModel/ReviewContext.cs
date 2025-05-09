d9f1f47c10c06ebd76f9237298b7c040dba04288
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-09 12:48:32 +0300

    add product review entities and context for business logic

---
+++ eUseControl.BusinessLogic/DBModel/ReviewContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using eUseControl.Domain.Entities.Review;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    public class ReviewContext : DbContext
+    {
+        public ReviewContext() : base("name=eUseControl")
+        {
+        }
+
+        public virtual DbSet<ReviewDbTable> Reviews { get; set; }
+    }
+}
