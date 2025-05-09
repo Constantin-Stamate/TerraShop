1eef5bec09352bbcf7423c321ece4655a0087262
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-09 12:33:49 +0300

    implement methods for managing products and product reviews

---
+++ eUseControl.BusinessLogic/Interfaces/IReview.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using eUseControl.Domain.Entities.Review;
+
+namespace eUseControl.BusinessLogic.Interfaces
+{
+    public interface IReview
+    {
+        ReviewResp CreateReview(ReviewData data, int userId);
+
+        List<ReviewData> GetReviewsByProductId(int productId);
+
+        ReviewResp DeleteReview(int reviewId);
+
+        ReviewData GetReviewById(int? reviewId);
+
+        ReviewResp UpdateReview(ReviewData data);
+    }
+}
