9e7e33e5c552f5fd6a6adc0597218b601c78a7c3
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-09 14:03:49 +0300

    integrate product functionality logic with frontend

--- eUseControl.Web/Controllers/ProductController.cs
+++ eUseControl.Web/Controllers/ProductController.cs
@@ -1,23 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AutoMapper;
 using eUseControl.BusinessLogic;
 using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities;
 using eUseControl.Domain.Entities.Product;
-using eUseControl.Web.Models;
+using eUseControl.Domain.Entities.Profile;
+using eUseControl.Domain.Entities.Review;
+using eUseControl.Web.Models.Product;
+using eUseControl.Web.Models.User;
+using eUseControl.Web.Models.Profile;
+using eUseControl.Web.Models.Review;
 
 namespace eUseControl.Web.Controllers
 {
     public class ProductController : BaseController
     {
         private readonly IProduct _product;
+        private readonly ISession _session;
+        private readonly IReview _review;
+        private readonly IProfile _profile;
 
         public ProductController()
         {
             var bl = new BusinessLogicManager();
             _product = bl.GetProductBL();
+            _session = bl.GetSessionBL();
+            _review = bl.GetReviewBL();
+            _profile = bl.GetProfileBL();
         }
 
         [HttpGet]
@@ -28,14 +42,23 @@ namespace eUseControl.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult AddProduct(Product product, HttpPostedFileBase ProductImageUrl)
+        public ActionResult AddProduct(Product product, HttpPostedFileBase productImageUrl)
         {
             if (ModelState.IsValid)
             {
-                if (ProductImageUrl != null && ProductImageUrl.ContentLength > 0)
+                if (productImageUrl != null && productImageUrl.ContentLength > 0)
                 {
-                    string fileName = Path.GetFileName(ProductImageUrl.FileName);
-                    string uploadsPath = Server.MapPath("~/Uploads/");
+                    var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" };
+                    var extension = Path.GetExtension(productImageUrl.FileName).ToLower();
+
+                    if (!allowedExtensions.Contains(extension))
+                    {
+                        TempData["ErrorMessage"] = "Invalid image format!";
+                        return RedirectToAction("AddProduct", "Product", new { error = true });
+                    }
+
+                    string fileName = Path.GetFileName(productImageUrl.FileName);
+                    string uploadsPath = Server.MapPath("~/Uploads/products/");
 
                     if (!Directory.Exists(uploadsPath))
                     {
@@ -48,7 +71,7 @@ namespace eUseControl.Web.Controllers
                     {
                         try
                         {
-                            ProductImageUrl.SaveAs(filePath);
+                            productImageUrl.SaveAs(filePath);
                         }
                         catch (Exception ex)
                         {
@@ -57,7 +80,12 @@ namespace eUseControl.Web.Controllers
                         }
                     }
 
-                    product.ProductImageUrl = "~/Uploads/" + fileName;
+                    product.ProductImageUrl = "~/Uploads/products/" + fileName;
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = "Product image is required!";
+                    return RedirectToAction("AddProduct", "Product", new { error = true });
                 }
 
                 var config = new MapperConfiguration(cfg =>
@@ -71,15 +99,20 @@ namespace eUseControl.Web.Controllers
                 var cookie = Request.Cookies["X-KEY"].Value;
                 if (string.IsNullOrEmpty(cookie))
                 {
-                    TempData["ErrorMessage"] = "You are not authenticated!";
                     return RedirectToAction("Login", "Login", new { error = true });
                 }
 
-                var result = _product.CreateProduct(productData, cookie);
+                var user = _session.GetUserByCookie(cookie);
+                if (user == null)
+                {
+                    return RedirectToAction("Login", "Login", new { error = true });
+                }
+
+                var result = _product.CreateProduct(productData, user.Id);
 
                 if (result.Status)
                 {
-                    TempData["SuccessMessage"] = "The product has been successfully created!";
+                    TempData["SuccessMessage"] = result.StatusMsg;
                     return RedirectToAction("AddProduct", "Product", new { success = true });
                 }
                 else
@@ -103,16 +136,19 @@ namespace eUseControl.Web.Controllers
             var cookie = Request.Cookies["X-KEY"].Value;
             if (string.IsNullOrEmpty(cookie))
             {
-                TempData["ErrorMessage"] = "You are not authenticated!";
                 return RedirectToAction("Login", "Login", new { error = true });
             }
 
-            var productData = _product.GetProductById(Id, cookie);
+            var user = _session.GetUserByCookie(cookie);
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Login", new { error = true });
+            }
 
+            var productData = _product.GetProductById(Id);
             if (productData == null)
             {
-                TempData["ErrorMessage"] = "Product not found!";
-                return RedirectToAction("UpdateProduct", "Product");
+                return RedirectToAction("ArticlesProfile", "Profile", new { error = true });
             }
 
             var config = new MapperConfiguration(cfg =>
@@ -128,14 +164,23 @@ namespace eUseControl.Web.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult UpdateProduct(Product product, HttpPostedFileBase ProductImageUrl, int Id)
+        public ActionResult UpdateProduct(Product product, HttpPostedFileBase productImageUrl)
         {
             if (ModelState.IsValid)
             {
-                if (ProductImageUrl != null && ProductImageUrl.ContentLength > 0)
+                if (productImageUrl != null && productImageUrl.ContentLength > 0)
                 {
-                    string fileName = Path.GetFileName(ProductImageUrl.FileName);
-                    string uploadsPath = Server.MapPath("~/Uploads/");
+                    var allowedExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif" };
+                    var extension = Path.GetExtension(productImageUrl.FileName)?.ToLower();
+
+                    if (!allowedExtensions.Contains(extension))
+                    {
+                        TempData["ErrorMessage"] = "Invalid image format!";
+                        return RedirectToAction("UpdateProduct", "Product", new { error = true });
+                    }
+
+                    string fileName = Path.GetFileName(productImageUrl.FileName);
+                    string uploadsPath = Server.MapPath("~/Uploads/products/");
 
                     if (!Directory.Exists(uploadsPath))
                     {
@@ -148,7 +193,7 @@ namespace eUseControl.Web.Controllers
                     {
                         try
                         {
-                            ProductImageUrl.SaveAs(filePath);
+                            productImageUrl.SaveAs(filePath);
                         }
                         catch (Exception ex)
                         {
@@ -157,7 +202,7 @@ namespace eUseControl.Web.Controllers
                         }
                     }
 
-                    product.ProductImageUrl = "~/Uploads/" + fileName;
+                    product.ProductImageUrl = "~/Uploads/products/" + fileName;
                 }
 
                 var config = new MapperConfiguration(cfg =>
@@ -171,15 +216,21 @@ namespace eUseControl.Web.Controllers
                 var cookie = Request.Cookies["X-KEY"].Value;
                 if (string.IsNullOrEmpty(cookie))
                 {
-                    TempData["ErrorMessage"] = "You are not authenticated!";
                     return RedirectToAction("Login", "Login", new { error = true });
                 }
 
-                var result = _product.UpdateProduct(productData, cookie, Id);
+                var user = _session.GetUserByCookie(cookie);
+                if (user == null)
+                {
+                    return RedirectToAction("Login", "Login", new { error = true });
+                }
+
+                var result = _product.UpdateProduct(productData);
 
                 if (result.Status)
                 {
-                    return RedirectToAction("ArticlesProfile", "Profile", new { success = true });
+                    TempData["SuccessMessage"] = result.StatusMsg;
+                    return RedirectToAction("UpdateProduct", "Product", new { success = true });
                 }
                 else
                 {
@@ -197,9 +248,93 @@ namespace eUseControl.Web.Controllers
         }
 
         [HttpGet]
-        public ActionResult ProductDetails()
+        public ActionResult ProductDetails(int productId, int? reviewId)
         {
-            return View();
+            var cookie = Request.Cookies["X-KEY"].Value;
+            if (string.IsNullOrEmpty(cookie))
+            {
+                return RedirectToAction("Login", "Login", new { error = true });
+            }
+
+            var userMinimal = _session.GetUserByCookie(cookie);
+            if (userMinimal == null)
+            {
+                return RedirectToAction("Login", "Login", new { error = true });
+            }
+
+            var productData = _product.GetProductById(productId);
+            if (productData == null)
+            {
+                return RedirectToAction("Shop", "Shop", new { error = true });
+            }
+
+            var allReviews = _review.GetReviewsByProductId(productId);
+
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<ReviewData, ReviewCompact>();
+                cfg.CreateMap<ProfileData, ProfileMini>();
+                cfg.CreateMap<ProductData, Product>();
+                cfg.CreateMap<UserMinimal, UserCompact>();
+            });
+
+            var mapper = config.CreateMapper();
+
+            var reviewProfileDict = new Dictionary<ReviewCompact, ProfileMini>();
+
+            foreach (var reviewData in allReviews)
+            {
+                var review = mapper.Map<ReviewCompact>(reviewData);
+                var profileData = _profile.GetProfileByUserId(reviewData.UserId);
+                if (profileData != null)
+                {
+                    var profile = mapper.Map<ProfileMini>(profileData);
+                    reviewProfileDict.Add(review, profile);
+                }
+            }
+
+            var product = mapper.Map<Product>(productData);
+            var user = mapper.Map<UserCompact>(userMinimal);
+
+            ReviewCompact reviewToEdit;
+            if (reviewId.HasValue && reviewId != 0)
+            {
+                var reviewData = _review.GetReviewById(reviewId);
+                reviewToEdit = mapper.Map<ReviewCompact>(reviewData);
+            }
+            else
+            {
+                reviewToEdit = new ReviewCompact
+                {
+                    ProductId = productId,
+                    Username = user.Username,
+                    Email = user.Email
+                };
+            }
+
+            var model = new ProductDetailsViewModel
+            {
+                Product = product,
+                UserCompact = user,
+                ReviewCompact = reviewToEdit,
+                Reviews = reviewProfileDict
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangeProductStatus(int productId)
+        {
+            var cookie = Request.Cookies["X-KEY"].Value;
+            if (string.IsNullOrEmpty(cookie))
+            {
+                return RedirectToAction("Login", "Login", new { error = true });
+            }
+
+            _product.UpdateProductStatus(productId);
+            return RedirectToAction("Articlesprofile", "Profile", new { success = true });
         }
     }
 }
