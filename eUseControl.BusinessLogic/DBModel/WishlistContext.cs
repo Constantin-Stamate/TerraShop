8b10aa67019d4fec38e8415879166b66ac2ec119
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-19 13:15:08 +0300

    create table for wishlist products

---
+++ eUseControl.BusinessLogic/DBModel/WishlistContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using eUseControl.Domain.Entities.Wishlist;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    public class WishlistContext : DbContext
+    {
+        public WishlistContext() : base("name=eUseControl")
+        {
+        }
+
+        public virtual DbSet<WishlistDbTable> WishlistProducts { get; set; }
+    }
+}
---
+++ eUseControl.BusinessLogic/Migrations/Wishlist/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace eUseControl.BusinessLogic.Migrations.Wishlist
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<eUseControl.BusinessLogic.DBModel.WishlistContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            MigrationsDirectory = @"Migrations\Wishlist";
+        }
+
+        protected override void Seed(eUseControl.BusinessLogic.DBModel.WishlistContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
