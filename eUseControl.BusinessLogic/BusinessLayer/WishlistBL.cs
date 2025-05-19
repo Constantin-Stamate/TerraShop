d63025fba3ef04c52c38fd9e5a5efa15a3624e70
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-19 13:20:57 +0300

    add business logic layer for wishlist

---
+++ eUseControl.BusinessLogic/BusinessLayer/WishlistBL.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using eUseControl.BusinessLogic.Core;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities.Product;
+
+namespace eUseControl.BusinessLogic.BusinessLayer
+{
+    public class WishlistBL : UserApi, IWishlist
+    {
+        public void AddProductToWishlist(int userId, int productId)
+        {
+            AddProductToWishlistAction(userId, productId);
+        }
+
+        public List<ProductLite> GetAllWishlistProducts(int userId)
+        {
+            return GetAllWishlistProductsAction(userId);
+        }
+
+        public int GetWishlistCountByUserId(int userId)
+        {
+            return GetWishlistCountByUserIdAction(userId);
+        }
+
+        public void RemoveProductFromWishlist(int productId, int userId)
+        {
+            RemoveProductFromWishlistAction(productId, userId);
+        }
+
+        public List<int> GetWishlistProductIds(int userId)
+        {
+            return GetWishlistProductIdsAction(userId);
+        }
+    }
+}
