2b253beb6446861dbcc2d3b1d10dd2b72f4006b9
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-19 13:13:15 +0300

    add entity for wishlist products

---
+++ eUseControl.Domain/Entities/Wishlist/WishlistDbTable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace eUseControl.Domain.Entities.Wishlist
+{
+    [Table("WishlistProducts")]
+    public class WishlistDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "UserId")]
+        public int UserId { get; set; }
+
+        [Required]
+        [Display(Name = "ProductId")]
+        public int ProductId { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "AddedDate")]
+        public DateTime AddedDate { get; set; }
+    }
+}
