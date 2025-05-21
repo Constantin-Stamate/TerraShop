f8f06f05facadd79c04f17997f7a20c7063e24d2
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-21 23:13:18 +0300

    create discount coupons table and seed initial data

---
+++ eUseControl.BusinessLogic/DBModel/CouponContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using eUseControl.Domain.Entities.Cart;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    public class CouponContext : DbContext
+    {
+        public CouponContext() : base("name=eUseControl")
+        {
+        }
+
+        public virtual DbSet<Coupon> DiscountCoupons { get; set; }
+    }
+}
