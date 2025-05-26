8918e53692dc6697cbbe1d84f967125f96dcae53
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-26 12:30:48 +0300

    create table for customer orders

---
+++ eUseControl.BusinessLogic/Migrations/Order/202505251731196_IntegrateCustomerOrdersTable.cs
@@ -0,0 +1,37 @@
+﻿namespace eUseControl.BusinessLogic.Migrations.Order
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class IntegrateCustomerOrdersTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CustomerOrders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserId = c.Int(nullable: false),
+                        FirstName = c.String(nullable: false, maxLength: 30),
+                        LastName = c.String(nullable: false, maxLength: 30),
+                        DeliveryAddress = c.String(nullable: false, maxLength: 70),
+                        PhoneNumber = c.String(nullable: false, maxLength: 20),
+                        Email = c.String(nullable: false, maxLength: 30),
+                        Notes = c.String(maxLength: 500),
+                        PaymentMethod = c.String(nullable: false, maxLength: 50),
+                        OrderDate = c.DateTime(nullable: false),
+                        OrderStatus = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.CustomerOrders", "UserId", "dbo.Users");
+            DropIndex("dbo.CustomerOrders", new[] { "UserId" });
+            DropTable("dbo.CustomerOrders");
+        }
+    }
+}
---
+++ eUseControl.Domain/Entities/Order/OrderDbTable.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using eUseControl.Domain.Enums;
+
+namespace eUseControl.Domain.Entities.Order
+{
+    [Table("CustomerOrders")]
+    public class OrderDbTable
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "UserId")]
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual UDbTable User { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "DeliveryAddress")]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(20)]
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(30)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [StringLength(500)]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Notes")]
+        public string Notes { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "PaymentMethod")]
+        public string PaymentMethod { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "OrderDate")]
+        public DateTime OrderDate { get; set; }
+
+        [EnumDataType(typeof(OrderStatus))]
+        [Display(Name = "OrderStatus")]
+        public OrderStatus OrderStatus { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        [Display(Name = "TotalPrice")]
+        public decimal TotalPrice { get; set; }
+    }
+}
