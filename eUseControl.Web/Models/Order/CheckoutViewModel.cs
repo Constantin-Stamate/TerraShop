3ba3d953678949f9238eca85bb459b92250d3945
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-05-26 12:57:50 +0300

    add entities for order handling

---
+++ eUseControl.Web/Models/Order/CheckoutViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using eUseControl.Web.Models.Cart;
+
+namespace eUseControl.Web.Models.Order
+{
+    public class CheckoutViewModel
+    {
+        public OrderCompact Order { get; set; }
+
+        public List<CartCompact> CartItems { get; set; }
+    }
+}
---
+++ eUseControl.Web/Models/Order/OrderCompact.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eUseControl.Web.Models.Order
+{
+    public class OrderCompact
+    {
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "DeliveryAddress")]
+        public string DeliveryAddress { get; set; }
+
+        [Required]
+        [Phone]
+        [StringLength(20)]
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [StringLength(30)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [StringLength(500)]
+        [DataType(DataType.MultilineText)]
+        [Display(Name = "Notes")]
+        public string Notes { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "PaymentMethod")]
+        public string PaymentMethod { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue)]
+        [Display(Name = "TotalPrice")]
+        public decimal TotalPrice { get; set; }
+    }
+}
