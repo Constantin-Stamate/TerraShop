420feefd81adfd96f17457ffea5f64c6f607b108
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-06-01 21:17:26 +0300

    modify customer orders table

---
+++ eUseControl.BusinessLogic/Migrations/Order/202505311328410_AddCouponIdToCustomerOrders.cs
@@ -0,0 +1,19 @@
+﻿namespace eUseControl.BusinessLogic.Migrations.Order
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class AddCouponIdToCustomerOrders : DbMigration
+    {
+        public override void Up()
+        {  
+            AddColumn("dbo.CustomerOrders", "CouponId", c => c.Int());
+            CreateIndex("dbo.CustomerOrders", "CouponId");
+            AddForeignKey("dbo.CustomerOrders", "CouponId", "dbo.DiscountCoupons", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.CustomerOrders", "CouponId");
+        }
+    }
+}
