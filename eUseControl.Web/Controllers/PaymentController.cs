4a8c25bc3503cb223946d8522464cd823d46915f
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-06-04 13:25:12 +0300

    complete transaction functionality

--- eUseControl.Web/Controllers/PaymentController.cs
+++ eUseControl.Web/Controllers/PaymentController.cs
@@ -1,13 +1,79 @@
 ﻿using System.Web.Mvc;
+using AutoMapper;
+using eUseControl.BusinessLogic;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities.Payment;
+using eUseControl.Web.Models.Payment;
 
 namespace eUseControl.Web.Controllers
 {
-    public class PaymentController : Controller
+    public class PaymentController : BaseController
     {
-        [HttpGet]
-        public ActionResult Payment()
+        private readonly ISession _session;
+        private readonly ITransaction _transaction;
+        private readonly ICart _cart;
+
+        public PaymentController()
         {
-            return View();
+            var bl = new BusinessLogicManager();
+            _session = bl.GetSessionBL();
+            _transaction = bl.GetTransactionBL();
+            _cart = bl.GetCartBL();
+        }
+
+        [HttpGet]
+        public ActionResult Payment(int orderId)
+        {
+            var model = new TransactionCompact
+            {
+                OrderId = orderId
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ProcessTransaction(TransactionCompact transactionCompact)
+        {
+            if (ModelState.IsValid)
+            {
+                var cookie = Request.Cookies["X-KEY"]?.Value;
+                if (string.IsNullOrEmpty(cookie))
+                {
+                    return RedirectToAction("Login", "Login", new { error = true });
+                }
+
+                var user = _session.GetUserByCookie(cookie);
+                if (user == null)
+                {
+                    return RedirectToAction("Login", "Login", new { error = true });
+                }
+
+                var config = new MapperConfiguration(cfg =>
+                {
+                    cfg.CreateMap<TransactionCompact, TransactionData>();
+                });
+
+                var mapper = config.CreateMapper();
+                var transaction = mapper.Map<TransactionData>(transactionCompact);
+
+                var result = _transaction.ProcessPayment(transaction, user.Id);
+
+                if (result.Status)
+                {
+                    var clearResult = _cart.ClearCartItemsAfterOrder(user.Id);
+                    return RedirectToAction("OrderConfirmation", "Order", new { success = true, orderId = transactionCompact.OrderId });
+                }
+                else
+                {
+                    return RedirectToAction("OrderFailure", "Order", new { error = true, orderId = transactionCompact.OrderId });
+                }
+            }
+            else 
+            {
+                return RedirectToAction("Payment", "Payment", new { error = true, orderId = transactionCompact.OrderId });
+            }
         }
     }
 }
---
+++ eUseControl.Web/Models/Payment/TransactionCompact.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eUseControl.Web.Models.Payment
+{
+    public class TransactionCompact
+    {
+        [Required]
+        [Display(Name = "OrderId")]
+        public int OrderId { get; set; }
+
+        [Required]
+        [StringLength(70)]
+        [Display(Name = "Full Name")]
+        public string FullName { get; set; }
+
+        [Required]
+        [CreditCard]
+        [Display(Name = "CardNumber")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [RegularExpression(@"^([0-9]{2})\/(0[1-9]|1[0-2])$")]
+        [Display(Name = "ExpiryDate")]
+        public string ExpiryDate { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{3}$")]
+        [Display(Name = "Cvv")]
+        public string Cvv { get; set; }
+    }
+}
