622e92ed9b830be87d30ce3e931ec20b8be432e6
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-06-09 01:46:06 +0300

    add entities for frontend review display

---
+++ eUseControl.Web/Models/Main/ReviewProfileData.cs
@@ -0,0 +1,12 @@
+﻿using eUseControl.Web.Models.Profile;
+using eUseControl.Web.Models.Review;
+
+namespace eUseControl.Web.Models.Main
+{
+    public class ReviewProfileData
+    {
+        public ReviewMini Review { get; set; }
+
+        public ProfileMini Profile { get; set; }
+    }
+}
---
+++ eUseControl.Web/Models/Review/ReviewMini.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace eUseControl.Web.Models.Review
+{
+    public class ReviewMini
+    {
+        public string Review { get; set; }
+
+        public int Rating { get; set; }
+
+        public DateTime ReviewPostDate { get; set; }
+    }
+}
