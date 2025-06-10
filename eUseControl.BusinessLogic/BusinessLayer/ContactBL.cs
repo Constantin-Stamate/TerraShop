9a1785a9e76eac55a407cf7195f78145c3bf428b
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-06-10 23:11:34 +0300

    define service for contact requests

---
+++ eUseControl.BusinessLogic/BusinessLayer/ContactBL.cs
@@ -0,0 +1,14 @@
+﻿using eUseControl.BusinessLogic.Core;
+using eUseControl.BusinessLogic.Interfaces;
+using eUseControl.Domain.Entities.Contact;
+
+namespace eUseControl.BusinessLogic.BusinessLayer
+{
+    public class ContactBL : UserApi, IContact
+    {
+        public ContactResp SubmitContactRequest(ContactData contactData, int userId)
+        {
+            return SubmitContactRequestAction(contactData, userId);
+        }
+    }
+}
