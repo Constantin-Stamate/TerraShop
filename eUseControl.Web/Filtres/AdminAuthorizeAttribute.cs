fc5c0adb6450fc1281bcbab464258329e1dd5a9c
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-07-11 16:49:55 +0300

    implement authorization filter for admin role

---
+++ eUseControl.Web/Filtres/AdminAuthorizeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using eUseControl.Domain.Entities;
+using eUseControl.Domain.Enums;
+
+namespace eUseControl.Web.Filtres
+{
+    public class AdminAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var user = httpContext.Session["User"] as UserMinimal;
+
+            if (httpContext.Session == null || httpContext.Session["User"] == null)
+            {
+                return false;
+            }
+
+            return user?.Level == URole.Admin;
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Login/Login?error=true");
+        }
+    }
+}
