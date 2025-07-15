b0d290b1364aac60d44a783039b1e38f76380d9a
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-07-15 23:45:25 +0300

    finalize chatbot functionality integration

---
+++ eUseControl.Web/Controllers/ChatController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using eUseControl.BusinessLogic;
+using eUseControl.BusinessLogic.Interfaces;
+
+namespace eUseControl.Web.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly IChat _chat;
+
+        public ChatController()
+        {
+            var BL = new BusinessLogicManager();
+            _chat = BL.GetChatBL();
+        }
+
+        [HttpGet]
+        public ActionResult Chat()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Respond(string message)
+        {
+            var response = _chat.GetResponse(message);
+
+            return Json(new { responseText = response });
+        }
+    }
+}
