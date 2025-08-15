e54236c4cbf9885db9573dabcc0aad14486e271a
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-08-15 20:31:33 +0300

    create subscribers table

---
+++ eUseControl.BusinessLogic/DBModel/SubscriptionContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using eUseControl.Domain.Entities.Subscription;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    public class SubscriptionContext : DbContext
+    {
+        public SubscriptionContext() : base("name=eUseControl")
+        {
+        }
+
+        public virtual DbSet<SubscriptionDbTable> Subscribers { get; set; }
+    }
+}
