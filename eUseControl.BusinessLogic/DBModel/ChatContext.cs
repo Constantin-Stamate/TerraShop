e0986c4370442336fa40cdb3625aa136bb511c4d
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-08-15 17:10:15 +0300

    add database context for chat functionality

---
+++ eUseControl.BusinessLogic/DBModel/ChatContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using eUseControl.Domain.Entities.Chat;
+
+namespace eUseControl.BusinessLogic.DBModel
+{
+    public class ChatContext : DbContext
+    {
+        public ChatContext() : base("name=eUseControl")
+        {
+        }
+
+        public virtual DbSet<ChatDbTable> ChatMessages { get; set; }
+    }
+}
